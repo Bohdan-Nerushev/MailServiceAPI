834325fcf645bd9de3a97ebe7ad71d111f4ed372
Bohdan Nerushev <bnerushev@united-internet.de>
2026-03-13 10:43:59 +0100

    fix test config

--- tests/e2e/config.py
+++ tests/e2e/config.py
@@ -1,8 +1,8 @@
 import os
 
 # Basis-URL der API
-BASE_URL = os.getenv("BASE_URL", "http://172.18.6.162:80").rstrip("/")
-
+BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8090").rstrip("/")
+#BASE_URL = os.getenv("BASE_URL", "http://172.18.6.162:80").rstrip("/")
 # Präfix für Testbenutzer, um sie leichter finden und löschen zu können
 TEST_USER_PREFIX = os.getenv("TEST_USER_PREFIX", "test_e2e_")
 
